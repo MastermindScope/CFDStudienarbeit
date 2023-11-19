3b3225d413c56da46372d14080d1bc1c18d637c8
MastermindScope <81287997+MastermindScope@users.noreply.github.com>
2023-11-19 21:25:03 +0100

    code review with andreas

--- aufgabe1/createA.m
+++ aufgabe1/createA.m
@@ -1,12 +1,19 @@
 function A = createA(meshSize, k, h, P, L, A_t, T_w, T_ar)
     dx = L/meshSize;
     m = h*P/(k*A_t);
-    A = zeros(meshSize);
-    A = A+diag(-ones(meshSize-1,1)/dx,1);
-    A = A+diag(-ones(meshSize-1,1)/dx,-1);
-    A = A+diag(ones(meshSize,1)*(2/dx+m*dx),0);
+    A = sparse(meshSize,meshSize);
+    ons = ones(meshSize-1,1);
+    rowCol = linspace(1,meshSize-1,meshSize-1)';
+    A = A+sparse(rowCol,rowCol+ons,-ons/dx,meshSize,meshSize);
+    %A = A+sparse(diag(-ones(meshSize-1,1)/dx,1));
+    A = A+sparse(rowCol+ons,rowCol,-ons/dx,meshSize,meshSize);
+    %A = A+sparse(diag(-ones(meshSize-1,1)/dx,-1));
+    ons = ones(meshSize,1);
+    rowCol = linspace(1,meshSize,meshSize)';
+    A = A+sparse(rowCol,rowCol,ons*(2/dx+m*dx));
+    %A = A+sparse(diag(ones(meshSize,1)*(2/dx+m*dx),0));
     A(1,1) = 3/dx+m*dx;
     A(1,2) = -1/dx;
-    A(meshSize,meshSize) = h/k/(1+h*dx/(2*k));
+    A(meshSize,meshSize) = h/k/(1+h*dx/(2*k))+1/dx+m*dx;
     A(meshSize,meshSize-1) = -1/dx;
 end
--- aufgabe1/main.m
+++ aufgabe1/main.m
@@ -2,7 +2,6 @@
 %Andreas Wenger & Vinzenz Goetz
 %Units = [kg,m,°C]
 %
-%A*T+b = T', do this very often
 %functions:
 %[output1, output2, ...] = fuction(input1, input2, ...)
 %matrix = createA(meshSize, k, h, P, L, At, T_w, T_ar)
@@ -11,58 +10,78 @@
 %vector = sourceTerms(meshSize, L, P, k, h, At, T_ar, T_w)
 %[boolean, scalar] = residual(eps, vectorOld, vectorNew, norm)
 %graph = visualize(vectorx,vectorT)
+close all
 
-meshSize = input("Input Mesh Size >>>  ");
-eps = input("Input Max Residual >>>   ");
-if meshSize == 0
-    meshSize = 20;
-end
+%this defines mesh sized to be investigated
+meshSize = 5:5:1000;
 
-if eps == 0
-    eps = 1e-6;
-end
 
+%this section defines the constants used in the problem
 k = 100;
 h = 10;
 P = 0.4;
 L = 1;
 A_t = 0.01;
-T_w = 100;
-T_ar = 10;
-T0 = 3;
-dx = L/meshSize;
+T_w = 300;
+T_ar = 20;
+T0 = 20;
+%m is m^2 in this case, shorthand because a longer variable is hard to
+%write
 m = h*P/(k*A_t);
 
+%this is defining some constants which help in the analytical solution of
+%the problem
+mprime=sqrt(m);
+a = h*exp(mprime*L)+mprime*k*exp(mprime*L);
+bprime = h*exp(-mprime*L)-mprime*k*exp(-mprime*L);
+gam = (T_w-T_ar)/((1-a/bprime));
 
-A = createA(meshSize, k, h, P, L, A_t, T_w, T_ar);
-T = createFields(meshSize);
-T = initFields(T, T0);
-b = sourceTerms(meshSize, L, P, k, h, A_t, T_ar, T_w);
-x = linspace(0,L,meshSize);
 
-res = inf;
-count = 0;
+%this is for no convection on the face
+%gam = (T_w-T_ar)/(1+exp(-2*sqrt(m)*L));
 
-while (res ~= 1)
-    disp(count);
-    count = count + 1;
-    Tprime = T;
-    T = (A*T+b);
-    [res, beepBoop] = residual(eps, Tprime, T, 2);
-    if count > 100
-        res = 1;
-    end
+%this initializes the error vectors with the right length
+errAbs = zeros(1,length(meshSize));
+errRel = zeros(1,length(meshSize));
+
+for i = 1:length(meshSize)
+    %here, the system matrix as well as source terms are initialized
+    dx = L/meshSize(i);
+    %system matrix is created
+    A = createA(meshSize(i), k, h, P, L, A_t, T_w, T_ar);
+    %source terms are created
+    b = sourceTerms(meshSize(i), L, P, k, h, A_t, T_ar, T_w);
+    x = linspace(0,L,meshSize(i));
+    
+    %system is solved and T obtained
+    T = A\b;
+    plot(x,T);
+    hold on
+    
+    %calculate analytical solution for given mesh size
+    anaSol = gam.*exp(mprime.*x)-gam.*a./bprime.*exp(-mprime*x)+T_ar;
+    
+    %fill error vectors
+    errAbs(i) = abs(anaSol(end)-T(end));
+    errRel(i) = norm(anaSol(end)-T(end))/norm(anaSol(end));
+
 end
 
+%plot errors and the analytical solution
+plot(x,anaSol)
+xlabel("x [m]");
+ylabel("T [°C]");
+figure
+semilogx(meshSize, errAbs)
+title("Absolute error on the last point")
+xlabel("Mesh elements");
+ylabel("Absolute error");
+grid
+figure
+semilogx(meshSize, errRel)
+title("Relative error on the last point")
+xlabel("Mesh elements");
+ylabel("Relative error");
+grid
 
-plot(x,T)
-
-
-
-
-
-
-
-
-
-
+hold off
